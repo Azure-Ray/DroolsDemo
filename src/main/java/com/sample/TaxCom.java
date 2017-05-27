008a66dc8c72e4b08abdfe2437b88f738345cb56
AA <527912143@qq.com>
2017-05-28 02:04:07 +0800

    new demo

---
+++ src/main/java/com/sample/TaxCom.java
@@ -0,0 +1,10 @@
+package com.sample;
+
+import java.math.BigDecimal;
+
+public class TaxCom {
+	public static void writeTax(BigDecimal SellPrice) {
+		// I'm the new tax method.
+		System.out.println("I'm the new tax method."+SellPrice);
+	}
+}
